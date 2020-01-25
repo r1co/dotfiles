eb4a78f5f6a237d09410309041494efe8f72d4b8
r1co <r1co@post-box.cc>
2020-01-25 02:16:21 +0100

    tmux scripts

--- tmux/info-scripts/tmuxUptimeInfo.sh
+++ tmux/info-scripts/tmuxUptimeInfo.sh
@@ -4,11 +4,11 @@ uptimeInfoLinux(){
   UPTIME_HOURS=$( echo $(awk '{print $1}' /proc/uptime) / 3600 | bc)
   UPTIME_MIN=$( echo $(awk '{print $1}' /proc/uptime) / 60  | bc)
   UPTIME_MIN=$(echo "$UPTIME_MIN%60" | bc)
-  echo $UPTIME_HOURS:$UPTIME_MIN
+  echo " ($UPTIME_HOURS:$UPTIME_MIN)"
 }
 
 uptimeInfoOSX() {
-  echo "XX:XX"
+  echo ""
 }
 
 if [ "$(uname)" == "Darwin" ]; then
