f4da9002fc1351163e6e9768c12f81094f96425a
r1co <r1co@post-box.cc>
2023-03-14 00:07:44 +0100

    'update dot mod'

--- zsh/mods/dot.zsh
+++ zsh/mods/dot.zsh
@@ -11,9 +11,12 @@ function dot.status() {
 function dot.save(){
     if [ -n "${1+1}" ]; then
         cd $DOTFILES_ROOT
+        set -x
+        echo "$@"
         git add *
-        git commit -m "$@"
+        git commit -m "'$*'"
         git push
+        set +x
     else
         printRed "Please provide commit message"
     fi
