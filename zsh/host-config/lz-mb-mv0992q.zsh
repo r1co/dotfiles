3b6aa3343e0454fa6da74686c0e612d2ac9956ec
Enrico Weigelt <enrico.weigelt@lemonize.de>
2023-03-20 13:20:09 +0100

    add host config

--- zsh/host-config/lz-mb-mv0992q.zsh
+++ zsh/host-config/lz-mb-mv0992q.zsh
@@ -1,2 +1,23 @@
 source $HOME/.dotfiles/zsh/zshrc.zsh
 
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
+
+
+export PATH="$PATH:/usr/local/go/bin:$HOME/go/bin:/Users/enrico.weigelt/JOBS/NPM/lzdev/bin"
+
+# configure mmctl
+source <(mmctl completion zsh)
+
+# Load Angular CLI autocompletion.
+source <(ng completion script)
+
+
+export PATH="$HOME/.rd/bin:$PATH"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
+
